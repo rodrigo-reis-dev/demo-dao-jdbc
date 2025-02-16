c90440e92b693f1d08e8081988e7a6d2d6c62427
Rodrigo dos Santos <rodrigoreiscs@gmail.com>
2025-02-16 13:26:38 +0000

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Rodrigo", "rodrigo@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted, new id = " + newSeller.getId());
+
+		System.out.println("\n=== Test 5, seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Nadia");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
